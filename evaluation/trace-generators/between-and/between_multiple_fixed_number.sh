2fa956170687be69348a4d0e2d46173afd6ffd31
Wei DOU <wei.dou@uni.lu>
2015-04-23 13:33:22 +0200

    typo

--- evaluation/trace-generators/between-and/between_multiple_fixed_number.sh
+++ evaluation/trace-generators/between-and/between_multiple_fixed_number.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./trace_generator_between_multiple_fixed_number.py -s 'between A and B' -p 'always B' -i p32 -l 100000
+./trace_generator_between_multiple_fixed_number.py -s 'between A and B' -p 'always C' -i p32 -l 100000
 ./trace_generator_between_multiple_fixed_number.py -s 'between A at least 1000 tu and B at least 500 tu' -p 'never C' -i p33 -l 100000
 ./trace_generator_between_multiple_fixed_number.py -s 'between A and B' -p 'C responding at least 1000 tu D' -i p34 -l 100000
 ./trace_generator_between_multiple_fixed_number.py -s 'between A and B' -p 'never exactly 2 C' -i p35 -l 100000
